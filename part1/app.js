a4c6a2a76cc7f711a10a92e8bf9657e5c1a93b72
Fauwou <jonahkourbel@gmail.com>
2025-06-20 08:58:01 +0000

    [automatic commit] 2025-06-20 08:58:01 UTC 175.35.5.92

--- part1/app.js
+++ part1/app.js
@@ -127,7 +127,9 @@ app.get("/api/walkers/summary", async function(req, res, next)
 })
 
                                                                     /* end section */
-app.use('/', indexRouter);
+
+/*app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
