721de9485fbfb7e92020cd73f5a9519ca9bb129e
Fauwou <jonahkourbel@gmail.com>
2025-06-20 18:05:38 +0000

    [automatic commit] 2025-06-20 18:05:37 UTC 175.35.5.92

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const app = express();
+const mysql = require("mysql2/promise")
 const session = require("express-session") //added this
 
 // Middleware
