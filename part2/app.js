99adaab317b6018652bf0d1ada942946447b35eb
Fauwou <jonahkourbel@gmail.com>
2025-06-20 18:32:40 +0000

    [automatic commit] 2025-06-20 18:32:40 UTC 175.35.5.92

--- part2/app.js
+++ part2/app.js
@@ -66,7 +66,7 @@ app.post("/api/login", async function(req, res)//sends the login connection with
         })
     }
 }
- catch (errorM) {//
+ catch (errorM) {//error catcher 
     console.error("da login fail forever:", errorM)
     res.status(500).json({
         success: false,
