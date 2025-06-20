fff928c2f29569e25dbcae4bdbeade6d2359241e
Fauwou <jonahkourbel@gmail.com>
2025-06-20 16:47:09 +0000

    [automatic commit] 2025-06-20 16:47:08 UTC 175.35.5.92

--- part2/public/javascripts/logintime.js
+++ part2/public/javascripts/logintime.js
@@ -32,6 +32,6 @@ try {
           }
 catch (errorM) { //catch error for if the login doesn't work
     errorElementing.textContent = "Login could not succeed because it failed. Try again."
-    errorElementing.classList.remove("")
+    errorElementing.style.display = "block"
           }
         })
