23af206d27a028ea014295493b233864286d73ba
Fauwou <jonahkourbel@gmail.com>
2025-06-20 11:54:01 +0000

    [automatic commit] 2025-06-20 11:54:01 UTC 175.35.5.92

--- part1/app.js
+++ part1/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs');
 var path = require('path')
-var 
+var mysql = require("mysql")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
