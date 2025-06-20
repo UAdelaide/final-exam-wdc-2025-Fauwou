dbb12b35c17d9e1b3eef50904736052f89a77b2d
Fauwou <jonahkourbel@gmail.com>
2025-06-20 18:15:10 +0000

    [automatic commit] 2025-06-20 18:15:09 UTC 175.35.5.92

--- part2/app.js
+++ part2/app.js
@@ -73,8 +73,13 @@ app.post("/api/login", async function(req, res)
 })
 
 
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
-
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
