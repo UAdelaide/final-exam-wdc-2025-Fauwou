faca5168e39bb51b19bdfab34dc04a845ca5024e
Fauwou <jonahkourbel@gmail.com>
2025-06-20 16:32:05 +0000

    [automatic commit] 2025-06-20 16:32:05 UTC 175.35.5.92

---
+++ part2/public/javascripts/logintime.js
@@ -0,0 +1,30 @@
+
+
+
+    createApp({
+      data() {
+        return {
+          username: "", //given information by user when attempting to log in
+          password: "",
+          error: "",
+          message: 'Welcome to the Dog Walking Service!'
+        };
+      },
+      methods: {
+        async LOGINNOW() { //login function prepared to run on click
+          try {
+            var response = await fetch("/api/login", {
+                method: "POST",
+                headers: { "Content-Type" : "application/json" },
+                body: JSON.stringify({ username, password})
+            })
+          var dataplease = await response.json()
+
+
+          }
+          catch (errorM) { //catch error for if the login doesn't work
+            this.error = "Login could not succeed because it failed."
+          }
+        }
+      }
+    }).mount('#app');
